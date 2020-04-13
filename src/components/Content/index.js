48ce2c963975b8f2f100aee1fe81bb92fa80c23f
Samuel Escapa <samuel.escapa@gmail.com>
2020-04-12 19:14:33 -0700

    Updates

--- src/components/Content/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Content.scss";
-
-function Content() {
-  return <div>Otran </div>;
-}
-
-export default Content;
--- src/components/Footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer.scss";
-
-function Footer() {
-  return <div>Samu es muy chistoso</div>;
-}
-
-export default Footer;
