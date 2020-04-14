5f0721fe5b2ca141d2b4fe36e8c31d74bbafc68b
Samuel Escapa <samuel.escapa@gmail.com>
2020-04-13 22:38:58 -0700

    Updates

---
+++ precache-manifest.75f7dd25b8a34689481c6e323628cf00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261d83f15f1e69ef209e3df96de6ed52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af0dbd8cb98a226832e8",
+    "url": "/static/css/main.1999c20f.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0dbd8cb98a226832e8",
+    "url": "/static/js/main.bb731175.chunk.js"
+  },
+  {
+    "revision": "5aa8c4c68520706df100",
+    "url": "/static/js/runtime-main.d1594a52.js"
+  },
+  {
+    "revision": "f03c89d447ebfcda0e033005f54775dc",
+    "url": "/static/media/JadeLogo.f03c89d4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.397792924670c3c2b743a66a97eb77b7.js"
+  "/precache-manifest.75f7dd25b8a34689481c6e323628cf00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
