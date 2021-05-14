0a3ebb310fcb0ae0f15f2c652daf14b2bdf631b7
Samuel Escapa <sescapa@zendesk.com>
2021-05-14 00:33:47 -0700

    Update: Add PDFs

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -5,11 +5,12 @@ function Content() {
   return (
     <div className="content">
       <div className="announcement">
-        Due to the shelter-in-place, we are moving our classes online until
-        further notice. We are currently adjusting to the new online system, and
-        we will be reaching out to our current students soon. If you are
-        interested in enrolling in any of our programs, please give us a call or
-        send us an email.
+        Jade Beauty and Barber College is now open for hybrid learning. Students
+        can learn theory online and come in person for practical lessons. We
+        follow Covid-19 safety protocols and cleaning methods in our school,
+        which all students must adhere to if they enter school premises. If you
+        are interested in enrolling in any of our programs, the best way to
+        reach us is by calling.
       </div>
       <br />
       Questions? Reach us at team<code>@</code>jadebeautybarbercollege.com or at
@@ -17,11 +18,107 @@ function Content() {
       <br />
       <br />
       <a
+        href="https://www.bppe.ca.gov"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        BPPE Website
+      </a>
+      <br />
+      <a
         href="https://www.bppe.ca.gov/webapps/annual_report_2018.php?schoolCode=90323886"
         target="_blank"
+        rel="noopener noreferrer"
       >
         BPPE Annual Report
       </a>
+      <br />
+      <a
+        href="/2021 Jade School's Catalogue.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        School Catalogue
+      </a>
+      <br />
+      <div className="title">Brochures</div>
+      <ul>
+        <li>
+          <a
+            href="/brochures/Cosmetology Brochure.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cosmetology Brochure
+          </a>
+        </li>
+        <li>
+          <a
+            href="/brochures/Barber Brochure.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Barber Brochure
+          </a>
+        </li>
+        <li>
+          <a
+            href="/brochures/Nail Brochure.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nail Care Brochure
+          </a>
+        </li>
+        <li>
+          <a
+            href="/brochures/Esthethician Brochure.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Skin Care Brochure
+          </a>
+        </li>
+      </ul>
+      <div className="title">Performance Sheets</div>
+      <ul>
+        <li>
+          <a
+            href="/performance sheets/2019 COSMETOLOGY performance sheet.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cosmetology Performance Sheet
+          </a>
+        </li>
+        <li>
+          <a
+            href="/performance sheets/2019 BARBER performance sheet.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Barber Performance Sheet
+          </a>
+        </li>
+        <li>
+          <a
+            href="/performance sheets/2019 NAIL CARE performance sheet.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nail Care Performance Sheet
+          </a>
+        </li>
+        <li>
+          <a
+            href="/performance sheets/2019 SKIN CARE performance sheet.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Skin Care Performance Sheet
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
